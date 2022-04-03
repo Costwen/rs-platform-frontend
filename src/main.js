67b87c86dbbd05e9a3704a0b74dfb4746e2779ec
costwen <1148796268@qq.com>
2022-04-03 10:35:25 +0800

    feat:add element-ui support

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import api from './api'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 Vue.prototype.$api = api
-
+Vue.use(ElementUI)
 new Vue({
   router,
   render: h => h(App)
