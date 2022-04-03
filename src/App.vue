1762114686ee7b63b2b99730790dcdfeb0a2888f
BUAAserein <18376359@buaa.edu.cn>
2022-04-03 17:34:51 +0800

    基本页面

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link> |-->
+<!--      <router-link to="/AnalysisTools">AnalysisTools</router-link>-->
+<!--    </div>-->
     <router-view/>
   </div>
 </template>
