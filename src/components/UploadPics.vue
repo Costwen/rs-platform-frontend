754057bd94acc61c36e9716e9bf39ef1bbd3f7b5
costwen <1148796268@qq.com>
2022-04-04 14:09:13 +0800

    feat: add map display

--- src/components/UploadPics.vue
+++ src/components/UploadPics.vue
@@ -28,55 +28,55 @@
 </template>
 
 <script>
-import axois from "axios";
+import axois from 'axios'
 
 export default {
-  name: "UploadPics",
+  name: 'UploadPics',
   props: [],
-  data() {
+  data () {
     return {
-      dialogImageUrl: "",
+      dialogImageUrl: '',
       dialogVisible: false,
-      imgUrl: "",
-      fileDate: "",
-      fileList: [],
-    };
+      imgUrl: '',
+      fileDate: '',
+      fileList: []
+    }
   },
   methods: {
     // 上传之前只需要检查图片的大小, auto-upload=false时，此函数无效
-    beforeUpload(file) {
+    beforeUpload (file) {
       if (file.size > 1024 * 1024) {
-        this.$message.error("上传图片不能大于1M");
-        return false;
+        this.$message.error('上传图片不能大于1M')
+        return false
       }
-      console.log(this.fileList);
-      return true;
+      console.log(this.fileList)
+      return true
     },
     // 处理变化
-    handleChange(file, fileList) {
-      this.fileList = fileList;
+    handleChange (file, fileList) {
+      this.fileList = fileList
     },
-    handleRemove(file, fileList) {
-      this.fileList = fileList;
+    handleRemove (file, fileList) {
+      this.fileList = fileList
     },
-    handleExceed(files, fileList) {
-      this.$message.error("最多只能上传1张图片");
+    handleExceed (files, fileList) {
+      this.$message.error('最多只能上传1张图片')
     },
     // 传给后端
-    submitUpload() {
-      let formData = new FormData();
-      console.log(this.fileList);
-      formData.append("file", this.fileList);
+    submitUpload () {
+      const formData = new FormData()
+      console.log(this.fileList)
+      formData.append('file', this.fileList)
       axois
-        .post("/image/retrieval/", {
-          img: this.fileList,
+        .post('/image/retrieval/', {
+          img: this.fileList
         })
         .then((res) => {
-          console.log(res);
-        });
-    },
-  },
-};
+          console.log(res)
+        })
+    }
+  }
+}
 </script>
 
 <style scoped></style>
---
+++ src/components/olmap.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="map" ref="rootmap">
+
+    </div>
+</template>
+
+<script>
+import 'ol/ol.css'
+import { Map, View } from 'ol'
+import TileLayer from 'ol/layer/Tile'
+import XYZ from 'ol/source/XYZ'
+// import mapconfig from '@config/mapconfig.js'
+export default {
+  data () {
+    return {
+      map: null
+    }
+  },
+  mounted () {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://webst01.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}&scl=1',
+            wrapX: false
+          })
+        }),
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://wprd01.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=8&x={x}&y={y}&z={z}&scl=1&ltype=4',
+            wrapX: false
+          })
+        })
+      ],
+      view: new View({
+        projection: 'EPSG:4326', // 使用这个坐标系
+        center: [116.397468, 39.908816], // 北京
+        maxZoom: 18,
+        zoom: 17
+      })
+    })
+  }
+}
+</script>
+
+<style>
+#map{height:100%;}
+/*隐藏ol的一些自带元素*/
+.ol-attribution,.ol-zoom { display: none;}
+
+</style>
