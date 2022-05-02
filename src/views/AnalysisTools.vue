423f80e5af6ee000c77d0f02235efe43ba8be64e
BUAAserein <18376359@buaa.edu.cn>
2022-05-02 11:24:30 +0800

    add echarts

--- src/views/AnalysisTools.vue
+++ src/views/AnalysisTools.vue
@@ -45,14 +45,73 @@
         </el-main>
       </el-container>
     </el-container>
+    <div id="chart-1" style="width: 600px;height:400px;"></div>
   </div>
 </template>
 
 <script>
 import Header from '../components/Header'
+
 export default {
   name: 'AnalysisTools',
-  components: { Header }
+  components: { Header },
+  methods: {
+    genChart () {
+      console.log('111')
+      var myChart = this.$echarts.init(document.getElementById('chart-1'))
+
+      // 指定图表的配置项和数据
+      var option = {
+        tooltip: {
+          trigger: 'item'
+        },
+        legend: {
+          top: '5%',
+          left: 'center'
+        },
+        series: [
+          {
+            name: 'Access From',
+            type: 'pie',
+            radius: ['40%', '70%'],
+            avoidLabelOverlap: false,
+            itemStyle: {
+              borderRadius: 10,
+              borderColor: '#fff',
+              borderWidth: 2
+            },
+            label: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: '40',
+                fontWeight: 'bold'
+              }
+            },
+            labelLine: {
+              show: false
+            },
+            data: [
+              { value: 1048, name: 'Search Engine' },
+              { value: 735, name: 'Direct' },
+              { value: 580, name: 'Email' },
+              { value: 484, name: 'Union Ads' },
+              { value: 300, name: 'Video Ads' }
+            ]
+          }
+        ]
+      }
+
+      // 使用刚指定的配置项和数据显示图
+      myChart.setOption(option)
+    }
+  },
+  mounted () {
+    this.genChart()
+  }
 }
 </script>
 
