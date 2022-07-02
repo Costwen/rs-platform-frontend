6fa4382ebe736bbe32145229b4707f7ddf0484e6
costwen <1148796268@qq.com>
2022-07-02 17:50:27 +0800

    feat: change api

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
         // target: 'http://127.0.0.1:8000/',
         changeOrigin: true,
         pathRewrite: {
-          '^/api': '/'
+          '^/api': '/api'
         }
       },
       '/ws': {
