dde60bd0bb71829d44c741297ed5e503ea707c3c
costwen <1148796268@qq.com>
2022-07-08 12:36:34 +0800

    fix: fix preview list bug

--- src/views/Image.vue
+++ src/views/Image.vue
@@ -181,6 +181,7 @@ export default {
       // 添加到第一个
       this.editMap[image.id] = false
       this.imageList.unshift(image)
+      this.s
       this.page_num = Math.ceil(this.imageList.length / this.page_size)
     },
     downloadIamge (imgsrc, name) { // 下载图片地址和图片名
